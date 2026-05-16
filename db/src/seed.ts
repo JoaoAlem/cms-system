8ac59767fdaf78d0a4ce974de2e77211fdc056a5
João Antonio Alem <67281961+JoaoAlem@users.noreply.github.com>
2026-05-16 14:34:13 -0300

    Feat/initial permissions (#5)
    * :sparkles: Feat | Adicionar banco de dados de RBAC
    
    * :sparkles: Feat | Adicionar script para adicionar novos usuários
    
    * :sparkles: Feat | Adicionar seed de permissões

--- db/src/seed.ts
+++ db/src/seed.ts
@@ -1,29 +1,61 @@
 import { NodePgDatabase } from "drizzle-orm/node-postgres"
-import { seed } from "drizzle-seed"
-import * as schema from "./db/schema"
+import { permissions, rolePermissions, roles } from "./db/schema"
 import { db } from "./index"
 
 async function main() {
-    await seedUser(db)
+    const permissionsAndRoles = await Promise.all([
+        seedInitialPermissions(db),
+        seedInitialRoles(db)
+    ])
+
+    await seedRolesPermissions(db, permissionsAndRoles)
 }
 
-async function seedUser(db: NodePgDatabase) {
+async function seedInitialPermissions(db: NodePgDatabase) {
+    return await db.insert(permissions).values([
+        { name: "view_users" },
+        { name: "create_users" },
+        { name: "edit_users" },
+        { name: "delete_users" },
+        { name: "view_permissions" },
+        { name: "create_permissions" },
+        { name: "edit_permissions" },
+        { name: "delete_permissions" },
+        { name: "view_roles" },
+        { name: "create_roles" },
+        { name: "edit_roles" },
+        { name: "delete_roles" },
+    ]).onConflictDoNothing({
+        target: permissions.name
+    }).returning({ permissionId: permissions.id })
+}
 
-    await seed(db, { users: schema.users }).refine((f) => ({
-        users: {
-            columns: {
-                name: f.fullName(),
-                email: f.email(),
-                password: f.string({
-                    isUnique: false
-                }),
-                createdAt: f.default({ defaultValue: new Date() }),
-                updatedAt: f.default({ defaultValue: new Date() }),
-                deletedAt: f.default({ defaultValue: null }),
-            },
-            count: 20
+async function seedInitialRoles(db: NodePgDatabase) {
+    return await db.insert(roles).values([
+        { name: "admin" },
+    ]).onConflictDoNothing({
+        target: roles.name
+    }).returning({ roleId: roles.id })
+}
+
+type permissionsAndRoles = [{
+    permissionId: number;
+}[], {
+    roleId: number;
+}[]]
+
+async function seedRolesPermissions(db: NodePgDatabase, permissionsAndRoles: permissionsAndRoles) {
+    const permissions = permissionsAndRoles[0]
+    const roles = permissionsAndRoles[1]
+
+    const values = permissions.map((permission) => {
+        return {
+            roleId: roles.at(0)?.roleId,
+            permissionId: permission.permissionId
         }
-    }));
+    })
+
+    return await db.insert(rolePermissions).values(values).onConflictDoNothing()
 }
 
 main();
