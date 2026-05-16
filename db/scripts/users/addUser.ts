ec97d46f227ae6f66d4f06ebb76f0276d948e043
joao <joao.aalem@gmail.com>
2026-05-16 13:55:17 -0300

    :sparkles: Feat | Adicionar script para adicionar novos usuários

---
+++ db/scripts/users/addUser.ts
@@ -0,0 +1,53 @@
+import argon2 from "argon2"
+import { db } from "../../src/index"
+import { users } from "../../src/db/schema"
+import { parseArgs } from "util";
+
+async function createNewUser(name: string, email: string, plainPassword: string) {
+    const hashedPassword = await argon2.hash(plainPassword);
+
+    await db.execute(`select setval('users_id_seq', (select max(id) from users));`)
+
+    await db.insert(users).values({
+        name,
+        email,
+        password: hashedPassword,
+    });
+}
+
+async function main() {
+    const { values } = parseArgs({
+        args: process.argv,
+        options: {
+            user: {
+                type: "string",
+                multiple: false,
+            },
+            email: {
+                type: "string",
+                multiple: false,
+            },
+            password: {
+                type: "string",
+                multiple: false,
+            },
+        },
+        strict: true,
+        allowPositionals: true,
+    });
+
+    if (!values?.user || !values.email || !values.password) {
+        throw new Error("Todos os argumentos são obrigatórios. Uso da função: bun --bun run scripts/users/addUser.ts --user {{user}} --email {{email}} --password {{password}}")
+    }
+    try {
+        await createNewUser(values.user, values.email, values.password)
+    } catch (e) {
+        console.error(e?.toString())
+        return 1;
+    }
+
+    console.log("Usuário criado com sucesso")
+    return 0;
+}
+
+await main()
