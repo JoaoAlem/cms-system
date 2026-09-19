c64b4720069da8be1a2f3b8092c8d3b74be81e98
João Alem <joao.aalem@gmail.com>
2026-09-19 20:54:25 -0300

    :sparkles: feat: encerrar o processo corretamente

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,9 +7,25 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+// graceful shutdown
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
+
