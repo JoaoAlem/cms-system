25d344a65e434f24716392cd3275a368ea9b48a4
João Alem <joao.aalem@gmail.com>
2026-09-19 22:02:37 -0300

    :sparkles: Feat: Adicionar rotas de autenticação e API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,12 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { auth } from "./lib/auth.js";
 
 const app = new Hono()
+
+app.all("/api/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
---
+++ api/src/lib/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { db } from "db";
+import argon2 from "argon2"
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "pg" or "mysql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 8,
+		maxPasswordLength: 256,
+    autoSignIn: true,
+    password: {
+			hash: async (password) => {
+          return await argon2.hash(password)
+			},
+			verify: async ({ hash, password }) => {
+        return await argon2.verify(hash, password)
+			}
+		}
+  },
+});
+
+export type AuthType = {
+  user: typeof auth.$Infer.Session.user | null;
+  session: typeof auth.$Infer.Session.session | null;
+};
