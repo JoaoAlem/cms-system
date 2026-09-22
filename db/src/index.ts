6f216171f00307a1bd9c4d147bfc7306fc2a2a43
João Alem <joao.aalem@gmail.com>
2026-09-22 19:16:16 -0300

    :sparkles: Feat: reestruturar pacote do banco de dados

--- db/src/index.ts
+++ db/src/index.ts
@@ -1,12 +1,1 @@
-import { config } from 'dotenv';
-config({ path: new URL('../.env', import.meta.url) });
-
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { relations } from './db/relations.js';
-
-export const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL!
-  },
-  relations: relations
-})
+export { db, db as default } from "./client.js";
